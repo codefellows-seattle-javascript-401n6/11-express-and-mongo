9ec833e7645ed9f91d2d6223bfe9f250b6ba914b
Rafael Malave <rafomalave@gmail.com>
2018-03-06 18:14:09 -0800

    intitial setup and create server

---
+++ lab-rafael/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Now listening to port ${PORT}`); 
+});
