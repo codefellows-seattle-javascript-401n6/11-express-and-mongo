ee55e1e258c2cf4b3a16c61738cb41c08f8b5066
rjtj2007 <teacherman1@gmail.com>
2018-03-06 19:26:04 -0800

    creating server and dependencies

---
+++ lab-ryan/server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log('listening on: ', PORT);
+});
