c6d294ba3481626bd0b584274e64cb5c1fac6d3c
alicialycan <alicialycan@gmail.com>
2018-03-10 17:18:23 -0800

    get request works, set up storage

--- lab-alicia/index.js
+++ lab-alicia/index.js
@@ -2,12 +2,13 @@
 
 const express = require('express');
 const app = express();
-const paddleAPI = require('./routes/paddleAPI.js')
+const paddleAPI = require('./api/paddleAPI.js');
+const Paddle = require('./models/paddle.js');
 
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-app.use('/api', paddleAPI);
+app.use('/api/paddles', paddleAPI);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
