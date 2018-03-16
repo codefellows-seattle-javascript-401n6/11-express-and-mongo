fc9c8446205d0196953479b6c686521f1c054f74
ixnp <enprocopios@gmail.com>
2018-03-16 16:03:28 -0700

    created cupcake class

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-//get /api/cupcakes
+const Senshi = require('./models/senshi.js');
+
 app.get(`/api/senshi`,(req,res)=>{
-    res.send('In the name of the moon!');
-})
+    const senshi = [new Senshi('Sailor Jupiter','  Makoto Kino',' Lightning Magic/Martial Arts',' Inner Senshi')]
+    res.send(senshi);
+});
 
 
 app.listen(PORT,() =>{
---
+++ models/senshi.js
@@ -0,0 +1,11 @@
+'use strict'
+class Senshi{
+    constructor(senshiName, name, power, rank){
+        this.senshiName = senshiName;
+        this.name = name;
+        this.power = power;
+        this.rank = rank;
+    }
+}
+
+module.exports = Senshi;
