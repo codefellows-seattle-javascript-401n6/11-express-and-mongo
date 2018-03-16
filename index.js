fba460a2dff9e4790d02eba80674e1afa4558e4c
ixnp <enprocopios@gmail.com>
2018-03-16 15:01:34 -0700

    Starting Over

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict'
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+//get /api/cupcakes
+app.get(`/api/cupcakes`,(req,res)=>{
+    res.send('In the name of the moon!');
+})
+
+
+app.listen(PORT,() =>{
+    console.log('http://localhost:'+ PORT);
+});
+
---
+++ routes/routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const Cupcake = require('../modles/cupdake');
+const mongoose = require('mongoose');
+
+app.get('/', (req,res) =>{
+ 
+})
+
+module.exports = router
